bdb0f360a39990da3c892116f3adf0985cfbe9d0
Daniil Kapustin <kadaniilo@gmail.com>
2022-01-21 16:50:31 +0300

    Current location, miles and register block, config file, icons

---
+++ configuration.js
@@ -0,0 +1,68 @@
+const CONFIGURATION = {
+    "locations": [
+        {
+            "title":"CORE - Echo Park",
+            "address1":"2044 Reservoir St",
+            "address2":"Los Angeles, CA 90026, USA",
+            "vaccination": true,
+            "preregisterLink": "https://forms.flowhealth.com/access/core-echopark",
+            "schedule": {
+                days: 'Mon - Sat',
+                hours: '8am-4pm'
+            },
+            "coords":{
+                "lat":34.07874527342286,
+                "lng":-118.2621040932541
+            },
+            "placeId":"ChIJa7z1hxnHwoARwSTReKDCppw"
+        },
+        {
+            "title":"CORE - First United Methodist Church",
+            "address1":"1025 S Long Beach Blvd",
+            "address2":"Compton, CA 90221, USA",
+            "mobile": true,
+            "schedule": {
+                days: 'Tue - Sat',
+                hours: '10am-6pm'
+            },
+            "preregisterLink": "https://forms.flowhealth.com/access/CORE-United-Methodist-Church",
+            "coords":{
+                "lat":33.88710179655561,
+                "lng":-118.20684176441803
+            },
+            "placeId":"ChIJ8Zl0sVjLwoARqjizxSEkpqQ"
+        },
+        {
+            "title":"CORE - LA Apparel Warehouse",
+            "address1":"1020 E 59th St",
+            "address2":"Los Angeles, CA 90001, USA",
+            "coords":{
+                "lat":33.986893553859176,
+                "lng":-118.25757990674592},
+            "placeId":"ChIJyVXxEVTIwoAR4Qxiq9xg1ew"
+        },
+        {
+            "title":"FH - Jefferson",
+            "address1":"2920 W Jefferson Blvd",
+            "address2":"Los Angeles, CA 90018, USA",
+            "coords":{
+                "lat":34.02550554014371,
+                "lng":-118.32790803558197
+            },
+            "placeId":"ChIJ3-ABRj-4woAR0Sz3BSwI57I"
+        },
+    ],
+    "mapOptions": {
+        "center":{
+            "lat":38.0,
+            "lng":-100.0
+        },
+        "fullscreenControl":true,
+        "mapTypeControl":false,
+        "streetViewControl":false,
+        "zoom":4,
+        "zoomControl":true,
+        "maxZoom":17
+    },
+    "mapsApiKey": "AIzaSyChLRDowBBnYvY16Zl2hLsWatW6mcpsuuo"
+};
