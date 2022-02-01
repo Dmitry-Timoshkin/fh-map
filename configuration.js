27f55c686287ea8166113ed9865db7b9935332d8
Daniil <kadaniilo@gmail.com>
2022-02-01 21:27:29 +0300

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -1,6 +1,6 @@
 const CONFIGURATION = {
     "locations": [
-        {
+       {
             "title":"CORE - Echo Park",
             "address1":"2044 Reservoir St",
             "address2":"Los Angeles, CA 90026, USA",
@@ -20,10 +20,9 @@ const CONFIGURATION = {
             "title":"CORE - First United Methodist Church",
             "address1":"1025 S Long Beach Blvd",
             "address2":"Compton, CA 90221, USA",
-            "mobile": true,
             "schedule": {
                 days: 'Tue - Sat',
-                hours: '10am-6pm'
+                hours: '10am-4pm'
             },
             "preregisterLink": "https://forms.flowhealth.com/access/CORE-United-Methodist-Church",
             "coords":{
@@ -33,23 +32,146 @@ const CONFIGURATION = {
             "placeId":"ChIJ8Zl0sVjLwoARqjizxSEkpqQ"
         },
         {
-            "title":"CORE - LA Apparel Warehouse",
-            "address1":"1020 E 59th St",
-            "address2":"Los Angeles, CA 90001, USA",
+            "title":"CORE - San Fernando Park",
+            "address1":"208 Park Ave",
+            "address2":"San Fernando, CA 91340, USA",
+            "schedule": {
+                days: 'Mon - Sat',
+                hours: '8am-4pm'
+            },
+            "preregisterLink": "https://forms.flowhealth.com/access/Carbon-LACity",
             "coords":{
-                "lat":33.986893553859176,
-                "lng":-118.25757990674592},
-            "placeId":"ChIJyVXxEVTIwoAR4Qxiq9xg1ew"
+                "lat":34.2807812,
+                "lng":-118.433943
+            },
+            "placeId":"?"
         },
         {
-            "title":"FH - Jefferson",
-            "address1":"2920 W Jefferson Blvd",
-            "address2":"Los Angeles, CA 90018, USA",
-            "coords":{
-                "lat":34.02550554014371,
-                "lng":-118.32790803558197
+            "title":"CORE - Lincoln Park",
+            "address1":"3501 Valley Blvd",
+            "address2":"Los Angeles, CA 90031, USA",
+            "mobile": true,
+            "schedule": {
+                days: 'Mon - Sat',
+                hours: '10am-4pm'
             },
-            "placeId":"ChIJ3-ABRj-4woAR0Sz3BSwI57I"
+            "preregisterLink": "https://forms.flowhealth.com/access/core-mobile",
+            "coords":{
+                "lat":34.0658942,
+                "lng":-118.1992006
+            },
+            "placeId":"?"
+        },
+        {
+            "title":"CORE - Mexican Consulate",
+            "address1":"2401 W 6th St",
+            "address2":"Los Angeles, CA 90057, USA",
+            "mobile": true,
+            "schedule": {
+                days: 'Mon - Fri',
+                hours: '10am-4pm'
+            },
+            "preregisterLink": "https://forms.flowhealth.com/access/core-mobile",
+            "coords":{
+                "lat":34.0611793,
+                "lng":-118.2780816
+            },
+            "placeId":"?"
+        },
+        {
+            "title":"CORE - Mid Valley YMCA",
+            "address1":"6901 Lennox Ave",
+            "address2":"Van Nuys, CA 91405, USA",
+            "mobile": true,
+            "schedule": {
+                days: 'Mon - Sat',
+                hours: '10am-4pm'
+            },
+            "preregisterLink": "https://forms.flowhealth.com/access/core-mobile",
+            "coords":{
+                "lat":34.1966584,
+                "lng":-118.446134
+            },
+            "placeId":"?"
+        },
+        {
+            "title":"CORE - Union Station (East Side)",
+            "address1":"811 N Vignes St",
+            "address2":"Los Angeles, CA 90012, USA",
+            "mobile": true,
+            "schedule": {
+                days: 'Mon - Sat',
+                hours: '9am-4pm'
+            },
+            "preregisterLink": "https://forms.flowhealth.com/access/core-unionstation",
+            "coords":{
+                "lat":34.0556383,
+                "lng":-118.232509
+            },
+            "placeId":"?"
+        },
+        {
+            "title":"CORE - YMCA East Valley",
+            "address1":"5142 Tujunga Ave",
+            "address2":"North Hollywood, CA 91601, USA",
+            "mobile": true,
+            "schedule": {
+                days: 'Mon - Sat',
+                hours: '10am-4pm'
+            },
+            "preregisterLink": "https://forms.flowhealth.com/access/core-mobile",
+            "coords":{
+                "lat":34.1641947,
+                "lng":-118.3785629
+            },
+            "placeId":"?"
+        },
+        {
+            "title":"CORE - Community Coalition",
+            "address1":"8101 Vermont Ave",
+            "address2":"Los Angeles, CA 90044, USA",
+            "mobile": true,
+            "schedule": {
+                days: 'Mon - Sat',
+                hours: '10am-4pm'
+            },
+            "preregisterLink": "https://forms.flowhealth.com/access/core-mobile",
+            "coords":{
+                "lat":33.9652205,
+                "lng":-118.2920968
+            },
+            "placeId":"?"
+        },
+        {
+            "title":"CORE - Strategy & Soul",
+            "address1":"3546 W Martin Luther King Jr Blvd",
+            "address2":"SLos Angeles, CA 90008, USA",
+            "mobile": true,
+            "schedule": {
+                days: 'Mon - Sat',
+                hours: '10am-4pm'
+            },
+            "preregisterLink": "https://forms.flowhealth.com/access/core-mobile",
+            "coords":{
+                "lat":34.0108,
+                "lng":-118.3343348
+            },
+            "placeId":"?"
+        },
+        {
+            "title":"CORE - PIERCE COLLEGE",
+            "address1":"Parking lot",
+            "address2":"Los Angeles, CA 91371, USA",
+            "schedule": {
+                days: 'Mon - Sat',
+                hours: '10am-4pm'
+            },
+            "preregisterLink": "https://forms.flowhealth.com/access/Core-PierceCollege",
+            "coords":{
+                "lat":34.1873702,
+                "lng":-118.5787023
+            },
+            "placeId":"?"
         },
     ],
     "mapOptions": {
